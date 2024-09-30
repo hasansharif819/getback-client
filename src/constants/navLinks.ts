62fa2dc76e0afcd77570058efd19f715214e21bf
hasansharif819 <hs.sharif819@gmail.com>
2024-09-30 21:51:35 +0600

    Navbar updated

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -2,8 +2,8 @@ const navLinks = [
 	{ title: 'Home', href: '/' },
 	{ title: 'Lost Items', href: '/lost-items' },
 	{ title: 'Found Items', href: '/found-items' },
-	{ title: 'About Us', href: '/about' },
-	{ title: 'Contact Us', href: '/contact' },
+	{ title: 'About', href: '/about' },
+	{ title: 'Contact', href: '/contact' },
 	{ title: 'FAQs', href: '/faqs' }
 ];
 
